d4b7b21b9fb16da8361fb5c5ef1543ba4f0a4dee
VietHung1 <hunghv@dolphin-technologies.net>
2024-04-22 14:36:49 +0700

    setup gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'http://memekhong.click/tonconnect-manifest.json';
+const manifestUrl = 'https://hung372001.github.io/Dapp/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <TonConnectUIProvider manifestUrl={manifestUrl}>
